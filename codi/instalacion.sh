7e9ec968b56eb8d1f5f744af6d4a9eda3c042f77
lluc <lluc@52dcc330-125d-4760-b35d-ad4847f4eaae>
2009-01-09 16:57:44 +0000

    Petits canvis a l'script d'instalacio
    git-svn-id: file:///home/backup/old_wekk.net/var/lib/svn/pca@9 52dcc330-125d-4760-b35d-ad4847f4eaae

--- codi/instalacion.sh
+++ codi/instalacion.sh
@@ -6,11 +6,6 @@ export BASE_DIR=$PWD
 export PATH_LLIB=$BASE_DIR
 export PATH_APLI=$BASE_DIR
 
-#cp TGZs/fftw-2.1.3.tar.gz $PATH_LLIB
-#cp TGZs/gnu_licensed_3D_Dock.tar.gz $PATH_APLI
-#cp TGZs/proteins.tar.gz $PATH_APLI
-#cp TGZs/ftdock_outputstests.tar.gz $PATH_APLI
-
 cd $PATH_LLIB
 tar -xvzf $BASE_DIR/TGZs/fftw-2.1.3.tar.gz
 cd fftw-2.1.3
@@ -24,10 +19,11 @@ tar -xvzf $BASE_DIR/TGZs/gnu_licensed_3D_Dock.tar.gz
 cd $PATH_APLI/3D_Dock/progs/
 tar -zxvf $BASE_DIR/TGZs/proteins.tar.gz
 tar -zxvf $BASE_DIR/TGZs/ftdock_outputstests.tar.gz
+make
 mkdir $BASE_DIR/output
-mv test1.output $BASE_DIR/outputs/test1.original
-mv test2.output $BASE_DIR/outputs/test2.original
-mv test3.output $BASE_DIR/outputs/test3.original
+mv test1.original $BASE_DIR/output/test1.original
+mv test2.original $BASE_DIR/output/test2.original
+mv test3.original $BASE_DIR/output/test3.original
 
 cd $BASE_DIR
 
